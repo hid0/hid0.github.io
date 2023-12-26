09056c03c2d0b21620ef1faa756bf01751bcd5d0
Faiz Hidayatulloh <faizhid11@gmail.com>
2023-12-26 17:33:27 +0000

    add some music

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline bg-slate-400">Hello world!</h1>
+    <>
+      <h1 className="text-3xl font-bold underline bg-slate-400">Hello world!</h1>
+      <iframe
+        style={{ borderRadius: 12 }}
+        src="https://open.spotify.com/embed/track/4ARcfREq6OpIVfVImAPFpc?utm_source=generator"
+        width="100%"
+        height={352}
+        allowFullScreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      />
+    </>
   );
 }
